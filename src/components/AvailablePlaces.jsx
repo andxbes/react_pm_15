0fc8e45a6c316db4192ef90f663ed804fd6c9702
andxbes <andxbes@gmail.com>
2024-06-24 19:14:29 +0300

    235

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Places from './Places.jsx';
 import { useEffect } from 'react';
+import Error from './Error.jsx'
 
 const places = localStorage.getItem('places');
 
@@ -9,17 +10,31 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setIsFetching(true);
-    fetch('http://localhost:3000/places').then((response) => {
+    fetch('http://localhost:3000/placesss').then((response) => {
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch places');
+      }
+
       return response.json();
     }).then((resData) => {
 
       setAvailablePlaces(resData.places);
       setIsFetching(false);
+    }).catch((error) => {
+      setError({ message: error.message || 'Could not fetch places, please try again later.' });
     });
   }, []);
+
+  if (error) {
+    return (
+      <Error title="An error occurred!" message={error.message}></Error>
+    );
+  }
 
   return (
     <Places
