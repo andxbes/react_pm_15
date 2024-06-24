9f06c5d6a027d94c7e69e87778b3ee452015a289
andxbes <andxbes@gmail.com>
2024-06-24 18:57:12 +0300

    234

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,13 +6,18 @@ const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
+
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     fetch('http://localhost:3000/places').then((response) => {
       return response.json();
     }).then((resData) => {
+
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     });
   }, []);
 
@@ -20,6 +25,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
