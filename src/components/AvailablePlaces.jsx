1018fde19c445a393e1cc3dfa447ae353024ed6e
Andrey Bescenniy <andxbes@gmail.com>
2024-06-24 11:13:45 +0300

    231

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
+import { useEffect } from 'react';
+
+const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      return response.json();
+    }).then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+  }, []);
+
   return (
     <Places
       title="Available Places"
